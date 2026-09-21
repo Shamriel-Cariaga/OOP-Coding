9eb0d46197b35f4c148a21de6d8ffef6887e2373
Shamriel-Cariaga <shamriel614@gmail.com>
2026-09-21 18:50:04 +0800

    Updated latest changes

--- src/activity9/Activity9.java
+++ src/activity9/Activity9.java
@@ -12,21 +12,24 @@ public class Activity9 {
         Scanner sc = new Scanner(System.in);
         int num = 1;
         
-        while(num != 0){
+        do{
             System.out.println("Enter a number: ");
-            num = sc.nextInt();
-            if(num==0){
+            num = sc.nextInt(0);
+            if(num == 0){
                 System.out.println("Goodbye!");
                 break;
             }
-        }
-   
-        if(num%2==0){
-            System.out.println("The number " + num + " is EVEN.");
-        }else
-            System.out.println("The number " + num + " is odd.");
-            
-        
+            if(num%2==0){
+                System.out.println("The number " + num + " is EVEN.");
+            }else{
+                System.out.println("The number " + num + " is odd.");
+            }
+            if(num < 0){
+                System.out.println("The number " + num + " is NEGATIVE.");
+            }else{
+                System.out.println("The number " + num + " is POSITIVE.");
+            }     
+        }while(num != 0);  
     }
     
 }
